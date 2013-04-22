c46da1be7e4ee9964e771b6750239cb0588dd8f3
Luke Benstead <lukeb@potatolondon.com>
2013-04-22 10:26:10 +0100

    Start implementing the filesystem outputter

--- outputters/filesystem.py
+++ outputters/filesystem.py
@@ -1,15 +1,43 @@
 import os
-from hashlib import md5
+import glob
+import logging
+from http import HttpResponse
+
 from ..base import Outputter
 
 class Filesystem(Outputter):
     def output(self, filename, file_out):
         content = file_out.read()
-        hsh = md5(content).hex_digest()
-        filename, ext = os.path.splitext(filename)
-        out_filename = os.path.join(self.directory, filename + "." + hsh + "." + ext)
-        open(out_filename, "w").write(content)
-        return out_filename
 
+        base, ext = os.path.splitext(filename)
 
+        already_exists = False
+        for f in glob.glob(base + "*" + ext):
+            if f == filename:
+                already_exists = True
+                continue
 
+            os.remove(f)
+
+        if not already_exists:
+            logging.error("Creating: %s", filename)
+
+            with open(filename, "w") as f:
+                f.write(content)
+
+    def file_up_to_date(self, filename):
+        return os.path.exists(filename)
+
+    def serve(self, filename):
+        base, ext = os.path.splitext(filename)
+
+        if ext == ".css":
+            mimetype = "text/css"
+        elif ext == ".js":
+            mimetype = "text/javascript"
+        else:
+            mimetype = "application/octet-stream"
+
+        content = open(filename, "r").read()
+
+        return HttpResponse(content, mimetype=mimetype)
